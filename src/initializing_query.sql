1544a97d7202146a6ef4271f56f7b4fac4151a07
Rob Gross <144big@gmail.com>
2017-06-27 16:01:33 -0400

    Added sql for database

---
+++ src/initializing_query.sql
@@ -0,0 +1,25 @@
+use myNewDB;
+
+drop table authorities;
+#create table users(
+#username varchar(50) not null,
+#password varchar(50) not null,
+##enabled boolean not null default true,
+#primary key(username)
+#);
+
+create table authorities(
+	username varchar(50) not null,
+    authority varchar(50) not null,
+    constraint fk_authorities_users foreign key(username) references
+    users(username)
+);
+
+insert into authorities (username,authority) values ("DaveWolf", "ADMIN");
+insert into authorities (username,authority) values ("1", "ADMIN");
+
+select * from users;
+select * from authorities;
+create unique index ix_auth_username on authorities (username, authority);
+
+select * from atm;
